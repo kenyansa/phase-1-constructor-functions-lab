a3e6b5e3964026942c5a8eb6f7b84c8205a9ffed
Kenyansa <kenyansajustine@gmail.com>
2022-04-28 07:44:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// objects
+// Scooter constructor: should create a new scooter with three properties:
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+// objects
+// Driver constructor:should create a new driver with three properties:
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+// objects
+// PickupLocation constructor:should create a new PickupLocation with two properties:
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
